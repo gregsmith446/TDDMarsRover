814c4ee9a21a6b0d3a7ed2e7e1f715501b80ab4a
Greg <gregsmith446@gmail.com>
2019-07-01 23:38:30 -0400

    sending the brute force version to the github

---
+++ MarsTravelingRover/MarsRoverMovementClass.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsTravelingRover
+{
+    class MarsRoverMovementClass
+    {
+        public int[] RovingRover(int[] location, char direction, int[] grid, string commands)
+        {
+
+            for(var i = 0; i < commands.Length; i++)
+            {
+                if (direction == 'e' && commands[i] == 'r')
+                    direction = 's';
+                else if (direction == 's' && commands[i] == 'r')
+                    direction = 'w';
+                else if (direction == 'w' && commands[i] == 'r')
+                    direction = 'n';
+                else if (direction == 'n' && commands[i] == 'r')
+                    direction = 'e';
+                else if(direction == 'e' && commands[i] == 'l')
+                    direction = 'n';
+                else if(direction == 'n' && commands[i] == 'l')
+                    direction = 'w';
+                else if (direction == 'w' && commands[i] == 'l')
+                    direction = 's';
+                else if (direction == 's' && commands[i] == 'l')
+                    direction = 'e';
+
+                if (direction == 'e')
+                {
+                    if (commands[i] == 'f')
+                    {   
+                        if(location[1] == 100)
+                            location[1] = 0;
+                        else
+                        location[1]++;
+                    }
+                    else if (commands[i] == 'b')
+                    {
+                        if (location[1] == 0)
+                            location[1] = 100;
+                        else
+                            location[1]--;
+                    }
+                }
+
+                if (direction == 's')
+                {
+                    if (commands[i] == 'f')
+                    {
+                        if (location[0] == 100)
+                            location[0] = 0;
+                        location[0]++;
+                    }
+                    else if (commands[i] == 'b')
+                    {
+                        if (location[0] == 0)
+                            location[0] = 100;
+                        else
+                            location[0]--;
+                    }
+                }
+
+                if (direction == 'n')
+                {
+                    if (commands[i] == 'f')
+                    {
+                        if (location[0] == 0)
+                            location[0] = 100;
+                        else
+                            location[0]--;
+                    }
+                    else if (commands[i] == 'b')
+                    {
+                        if (location[0] == 100)
+                            location[0] = 0;
+                        else
+                        location[0]++;
+                    }
+                }
+
+                if (direction == 'w')
+                {
+                    if (commands[i] == 'f')
+                    {
+                        if (location[1] == 0)
+                            location[1] = 100;
+                        else
+                            location[1]--;
+                    }
+                    else if (commands[i] == 'b')
+                    {
+                        if (location[1] == 100)
+                            location[1] = 0;
+                        else
+                        location[1]++;
+                    }
+                }
+            }
+            return location;
+        }
+
+    }
+}
---
+++ MarsTravelingRover/RoverTests.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MarsTravelingRover
+{
+    [TestFixture]
+    public class RoverTests
+    {
+        [Test]
+        public void givenAllInputsReturnAnIntArray()
+        {
+            int[] location = { };
+            char direction = 'e';
+            int[] grid = { };
+            string commands = "";
+
+            int[] expected = new int[] { };
+            int[] output = new MarsRoverMovementClass().RovingRover(location, direction, grid, commands);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void givenInputsWitNoCommandsReturnStartingLocation()
+        {
+            int[] location = {0, 0};
+            char direction = 'e';
+            int[] grid = {100, 100};
+            string commands = "ff";
+
+            int[] expected = new int[] {0, 2};
+            int[] output = new MarsRoverMovementClass().RovingRover(location, direction, grid, commands);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void givenInputsWithCommandBackReturnNewLocation()
+        {
+            int[] location = { 0, 0 };
+            char direction = 'e';
+            int[] grid = { 100, 100 };
+            string commands = "ffb";
+
+            int[] expected = new int[] { 0, 1 };
+            int[] output = new MarsRoverMovementClass().RovingRover(location, direction, grid, commands);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void givenInputsWithCommandRightReturnNewLocation()
+        {
+            int[] location = { 0, 0 };
+            char direction = 'e';
+            int[] grid = { 100, 100 };
+            string commands = "ffrf";
+
+            int[] expected = new int[] { 1, 2 };
+            int[] output = new MarsRoverMovementClass().RovingRover(location, direction, grid, commands);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void givenInputsWithCommandLeftReturnNewLocation()
+        {
+            int[] location = { 50, 50 };
+            char direction = 'e';
+            int[] grid = { 100, 100 };
+            string commands = "fflf";
+
+            int[] expected = new int[] { 49, 52  };
+            int[] output = new MarsRoverMovementClass().RovingRover(location, direction, grid, commands);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void givenMixedCommandsThatMakeASquareReturnToStartingLocationGoingRight()
+        {
+            int[] location = { 50, 50 };
+            char direction = 'e';
+            int[] grid = { 100, 100 };
+            string commands = "rfrfrfrf";
+
+            int[] expected = new int[] { 50, 50 };
+            int[] output = new MarsRoverMovementClass().RovingRover(location, direction, grid, commands);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void givenMixedCommandsThatMakeASquareReturnToStartingLocationGoingLeft()
+        {
+            int[] location = { 50, 50 };
+            char direction = 'e';
+            int[] grid = { 100, 100 };
+            string commands = "lflflflf";
+
+            int[] expected = new int[] { 50, 50 };
+            int[] output = new MarsRoverMovementClass().RovingRover(location, direction, grid, commands);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void givenInputsThatRequireWrappingOnXAxisReturnCorrectLocation()
+        {
+            int[] location = { 0, 0 };
+            char direction = 'w';
+            int[] grid = { 100, 100 };
+            string commands = "f";
+
+            int[] expected = new int[] { 0 , 100 };
+            int[] output = new MarsRoverMovementClass().RovingRover(location, direction, grid, commands);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void givenInputsThatRequireWrappingOnYAxisReturnCorrectLocation()
+        {
+            int[] location = { 0, 0 };
+            char direction = 'n';
+            int[] grid = { 100, 100 };
+            string commands = "f";
+
+            int[] expected = new int[] { 100, 0 };
+            int[] output = new MarsRoverMovementClass().RovingRover(location, direction, grid, commands);
+
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
